7d2cab9b9d4a0f0a6847e7370f9120d2989b0506
muzafarova <alisapsee@gmail.com>
2025-07-09 11:27:21 +0100

    chore: plug-in sarah's logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import Image from 'next/image';
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <Image
+      src="/logo.avif"
+      alt="Sarah Barlow Photography"
+      width={202}
+      height={202}
+    />
+  );
 }
