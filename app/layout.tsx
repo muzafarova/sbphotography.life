dbdd21cf5faac22a72ae24776125bde2ddecf3f1
muzafarova <alisapsee@gmail.com>
2025-07-10 11:57:51 +0100

    fix: typo in import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { Analytics } from '@vercel/analytics/next';
-import { SpeedInsights } from '@vercel/speed-insights/nuxt';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function RootLayout({
   children,
